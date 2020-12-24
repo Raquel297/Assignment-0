537d8459da13636538c94e4b17491efd5c50ecdd
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 04:21:24 -0500

    Update isPalindrome.js

--- Assignment-0-master/13-isPalindrome/isPalindrome.js
+++ Assignment-0-master/13-isPalindrome/isPalindrome.js
@@ -1,5 +1,15 @@
 function isPalindrome(word) {
   // Insert code here;
+  if(word.length === 1 || (word.length === 2 && word.charAt(0) === word.charAt(1))){
+    console.log(true);
+    return true;
+  }else if(word.charAt(0) !== word.charAt(word.length - 1)){
+    console.log(false);
+    return false;
+  }
+  word = word.substring(1, word.length - 1);
+  console.log(word);
+  isPalindrome(word);
 }
 
 // Do not edit this line;

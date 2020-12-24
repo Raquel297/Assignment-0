e252f3e3a2207e2840daf3ea5c2e9d85aa3c7cb0
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 06:28:08 -0500

    Update sumOfAllEvenNumbers.js

--- Assignment-0-master/08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ Assignment-0-master/08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -2,7 +2,7 @@ function sumOfAllEvenNumbers(nums) {
   // Insert code here;
   var count = 0; //This is our counter variable that will store all the instances of even numbers
   for(var i = 0; i < nums.length; i++){ //We loop through the entire array to check for even numbers
-    if(nums[i] % 2 == 0){ //If we found an even number, then we add that instance to our counter variable 
+    if(nums[i] % 2 === 0){ //If we found an even number, then we add that instance to our counter variable 
       count += 1;
     }
   }

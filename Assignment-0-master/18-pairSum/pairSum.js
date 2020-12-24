9fca96189da9520bc08cf2ea4d2580f67b492401
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 05:55:39 -0500

    Update pairSum.js

--- Assignment-0-master/18-pairSum/pairSum.js
+++ Assignment-0-master/18-pairSum/pairSum.js
@@ -1,5 +1,13 @@
 function pairSum(nums, target) {
   // Insert code here;
+  for(var i = 0; i < nums.length; i++){ 
+    for(var j = i + 1; j < nums.length; j++){
+      if(nums[i] + nums[j] === target){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;

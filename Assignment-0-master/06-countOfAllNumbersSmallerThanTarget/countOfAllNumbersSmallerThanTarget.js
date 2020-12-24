87b66addaf87358d93324ac562d23e7f579a53d5
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 03:17:17 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- Assignment-0-master/06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ Assignment-0-master/06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var count = 0; //This is our counter variable that will store all the instances of when a number in nums is less than our target
+  for(var i = 0; i < arr.length; i++){ //We loop through the entire array to check for such instances 
+    if(nums[i] < target){ //If the value in nums is less than the target value, then we add that instance to our counter variable 
+      count += 1;
+    }
+  }
+  return count; //We return our result
 }
 
 // Do not edit this line;

f11c35320b5004434795f4f573f3ef7cd4f7fea2
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 05:49:30 -0500

    Update setUnionOfAnyAmountOfSets.js

--- Assignment-0-master/17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ Assignment-0-master/17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  var union = new Set();
+  args.forEach(element => { //For each set 
+    element.forEach(value => { //For each element in each set 
+        if(!(value in union)){ //If the value does not already exist in the set...
+          union.add(value); //Add that value to the set
+        }
+    });
+  });
+  return union;
 }
 
 // Do not edit this line;

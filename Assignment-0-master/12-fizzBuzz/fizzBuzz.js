549c89978bbb727201474f95f1de611289e13598
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 03:38:43 -0500

    Update fizzBuzz.js

--- Assignment-0-master/12-fizzBuzz/fizzBuzz.js
+++ Assignment-0-master/12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  var array = []; //We create an empty output array where the fizz buzz or number values will be stored 
+  for(var i = start; i <= end; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+       array.push("FizzBuzz");
+    }else if(i % 3 == 0){
+      array.push("Fizz");
+    }else if(i % 5 == 0){
+      array.push("Buzz");
+    }else{
+      array.push(i);
+    }
+  }
+  return array; //We return the resulting array 
 }
 
 // Do not edit this line;

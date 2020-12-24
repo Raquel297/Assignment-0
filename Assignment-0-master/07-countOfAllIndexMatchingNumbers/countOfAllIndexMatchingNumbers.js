395f56ba9326b5bdcf49949783df661b33ed092b
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 06:27:49 -0500

    Update countOfAllIndexMatchingNumbers.js

--- Assignment-0-master/07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ Assignment-0-master/07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -2,7 +2,7 @@ function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
   var count = 0; //This is our counter variable that will store all the instances of a value being in its same index place
   for(var i = 0; i < nums.length; i++){ //We loop through the entire array to check for such instances 
-    if(nums[i] == i){ //If we found value in nums that is equal to its position, then we add that instance to our counter variable 
+    if(nums[i] === i){ //If we found value in nums that is equal to its position, then we add that instance to our counter variable 
       count += 1;
     }
   }

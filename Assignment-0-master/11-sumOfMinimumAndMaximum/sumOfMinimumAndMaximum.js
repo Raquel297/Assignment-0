095dca9dc5cb1926a55a77d8c7da415162cb0845
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 03:31:48 -0500

    Update sumOfMinimumAndMaximum.js

--- Assignment-0-master/11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ Assignment-0-master/11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,6 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  return Math.min(nums) + Math.max(nums); //We return the sum of the smallest and largest numbers in the nums array 
 }
 
 // Do not edit this line;

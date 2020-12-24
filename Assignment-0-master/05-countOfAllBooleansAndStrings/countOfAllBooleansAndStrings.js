7861b72ea8e5fc9330dd98c06b8dcbfd178707d4
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 06:27:27 -0500

    Update countOfAllBooleansAndStrings.js

--- Assignment-0-master/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ Assignment-0-master/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -2,7 +2,7 @@ function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
   var count = 0; //This is our counter variable that will store all the instances of boolean values or strings in the arr object
   for(var i = 0; i < arr.length; i++){ //We loop through the entire array to check for boolean values or strings
-    if(arr[i] == true || arr[i] == false || typeof arr[i] == 'string'){ //If we found a boolean value or string, then we add that instance to our counter variable 
+    if(arr[i] === true || arr[i] === false || typeof arr[i] === 'string'){ //If we found a boolean value or string, then we add that instance to our counter variable 
       count += 1;
     }
   }

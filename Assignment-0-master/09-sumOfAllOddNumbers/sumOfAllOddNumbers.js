b2189a9a5cad605222909de8285d1021973e459e
Raquel297 <60490785+Raquel297@users.noreply.github.com>
2020-12-24 03:25:41 -0500

    Update sumOfAllOddNumbers.js

--- Assignment-0-master/09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ Assignment-0-master/09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  var count = 0; //This is our counter variable that will store all the instances of odd numbers
+  for(var i = 0; i < nums.length; i++){ //We loop through the entire array to check for odd numbers
+    if(nums[i] % 2 != 0){ //If we found an odd number, then we add that instance to our counter variable 
+      count += 1;
+    }
+  }
+  return count; //We return our result
 }
 
 // Do not edit this line;
